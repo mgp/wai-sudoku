c2f8dbcb3301b37b103b9d67cfc9d5b0e2aaf17a
Kyle Morton <kmorton@ucla.edu>
2012-01-14 15:45:38 -0800

    adding UI

---
+++ tablefill.js
@@ -0,0 +1,110 @@
+function fillGrid()
+            {
+                var gridnum=[["0","0","0","0","0","0","0","0","0"],
+                                    ["0","0","0","0","0","0","0","0","0"],
+                                    ["0","0","0","0","0","0","0","0","0"],
+                                    ["0","0","0","0","0","0","0","0","0"],
+                                    ["0","0","0","0","0","0","0","0","0"],
+                                    ["0","0","0","0","0","0","0","0","0"],
+                                    ["0","0","0","0","0","0","0","0","0"],
+                                    ["0","0","0","0","0","0","0","0","0"],
+                                    ["0","0","0","0","0","0","0","0","0"]];
+                /*var i=0,j=0;
+                var row1='a';
+                var idobject=row1 + j;
+                        document.getElementById(idobject).innerHTML = gridnum[i][j];
+                        j=j+1;*/
+
+                document.getElementById('a1').innerHTML = gridnum[0][0];
+                document.getElementById('a2').innerHTML = gridnum[0][1];
+                document.getElementById('a3').innerHTML = gridnum[0][2];
+                document.getElementById('a4').innerHTML = gridnum[0][3];
+                document.getElementById('a5').innerHTML = gridnum[0][4];
+                document.getElementById('a6').innerHTML = gridnum[0][5];
+                document.getElementById('a7').innerHTML = gridnum[0][6];
+                document.getElementById('a8').innerHTML = gridnum[0][7];
+                document.getElementById('a9').innerHTML = gridnum[0][8];
+
+                document.getElementById('b1').innerHTML = gridnum[1][0];
+                document.getElementById('b2').innerHTML = gridnum[1][1];
+                document.getElementById('b3').innerHTML = gridnum[1][2];
+                document.getElementById('b4').innerHTML = gridnum[1][3];
+                document.getElementById('b5').innerHTML = gridnum[1][4];
+                document.getElementById('b6').innerHTML = gridnum[1][5];
+                document.getElementById('b7').innerHTML = gridnum[1][6];
+                document.getElementById('b8').innerHTML = gridnum[1][7];
+                document.getElementById('b9').innerHTML = gridnum[1][8];
+
+                document.getElementById('c1').innerHTML = gridnum[2][0];
+                document.getElementById('c2').innerHTML = gridnum[2][1];
+                document.getElementById('c3').innerHTML = gridnum[2][2];
+                document.getElementById('c4').innerHTML = gridnum[2][3];
+                document.getElementById('c5').innerHTML = gridnum[2][4];
+                document.getElementById('c6').innerHTML = gridnum[2][5];
+                document.getElementById('c7').innerHTML = gridnum[2][6];
+                document.getElementById('c8').innerHTML = gridnum[2][7];
+                document.getElementById('c9').innerHTML = gridnum[2][8];
+
+                document.getElementById('d1').innerHTML = gridnum[3][0];
+                document.getElementById('d2').innerHTML = gridnum[3][1];
+                document.getElementById('d3').innerHTML = gridnum[3][2];
+                document.getElementById('d4').innerHTML = gridnum[3][3];
+                document.getElementById('d5').innerHTML = gridnum[3][4];
+                document.getElementById('d6').innerHTML = gridnum[3][5];
+                document.getElementById('d7').innerHTML = gridnum[3][6];
+                document.getElementById('d8').innerHTML = gridnum[3][7];
+                document.getElementById('d9').innerHTML = gridnum[3][8];
+
+                document.getElementById('e1').innerHTML = gridnum[4][0];
+                document.getElementById('e2').innerHTML = gridnum[4][1];
+                document.getElementById('e3').innerHTML = gridnum[4][2];
+                document.getElementById('e4').innerHTML = gridnum[4][3];
+                document.getElementById('e5').innerHTML = gridnum[4][4];
+                document.getElementById('e6').innerHTML = gridnum[4][5];
+                document.getElementById('e7').innerHTML = gridnum[4][6];
+                document.getElementById('e8').innerHTML = gridnum[4][7];
+                document.getElementById('e9').innerHTML = gridnum[4][8];
+
+                document.getElementById('f1').innerHTML = gridnum[5][0];
+                document.getElementById('f2').innerHTML = gridnum[5][1];
+                document.getElementById('f3').innerHTML = gridnum[5][2];
+                document.getElementById('f4').innerHTML = gridnum[5][3];
+                document.getElementById('f5').innerHTML = gridnum[5][4];
+                document.getElementById('f6').innerHTML = gridnum[5][5];
+                document.getElementById('f7').innerHTML = gridnum[5][6];
+                document.getElementById('f8').innerHTML = gridnum[5][7];
+                document.getElementById('f9').innerHTML = gridnum[5][8];
+
+                document.getElementById('g1').innerHTML = gridnum[6][0];
+                document.getElementById('g2').innerHTML = gridnum[6][1];
+                document.getElementById('g3').innerHTML = gridnum[6][2];
+                document.getElementById('g4').innerHTML = gridnum[6][3];
+                document.getElementById('g5').innerHTML = gridnum[6][4];
+                document.getElementById('g6').innerHTML = gridnum[6][5];
+                document.getElementById('g7').innerHTML = gridnum[6][6];
+                document.getElementById('g8').innerHTML = gridnum[6][7];
+                document.getElementById('g9').innerHTML = gridnum[6][8];
+
+                document.getElementById('h1').innerHTML = gridnum[7][0];
+                document.getElementById('h2').innerHTML = gridnum[7][1];
+                document.getElementById('h3').innerHTML = gridnum[7][2];
+                document.getElementById('h4').innerHTML = gridnum[7][3];
+                document.getElementById('h5').innerHTML = gridnum[7][4];
+                document.getElementById('h6').innerHTML = gridnum[7][5];
+                document.getElementById('h7').innerHTML = gridnum[7][6];
+                document.getElementById('h8').innerHTML = gridnum[7][7];
+                document.getElementById('h9').innerHTML = gridnum[7][8];
+
+                document.getElementById('i1').innerHTML = gridnum[8][0];
+                document.getElementById('i2').innerHTML = gridnum[8][1];
+                document.getElementById('i3').innerHTML = gridnum[8][2];
+                document.getElementById('i4').innerHTML = gridnum[8][3];
+                document.getElementById('i5').innerHTML = gridnum[8][4];
+                document.getElementById('i6').innerHTML = gridnum[8][5];
+                document.getElementById('i7').innerHTML = gridnum[8][6];
+                document.getElementById('i8').innerHTML = gridnum[8][7];
+                document.getElementById('i9').innerHTML = gridnum[8][8];
+
+
+
+            }
