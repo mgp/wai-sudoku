8106f7a5d8ef9fbf3918fb6d430de97542a19ac4
Game Squad <gamesquad@ed-xps.math.ucla.edu>
2012-01-14 23:33:47 -0800

    added additional controls

--- jinput.js
+++ jinput.js
@@ -1,9 +1,34 @@
            
 
 	 $(function() {
+		var controlSwitch = false;
+		var controlSwitchRCB = "x";
+		var tCounter = 0;
+		
                 $(window).keydown(function(e) {
-                   switch(e.which){
-                        case 37:
+		   if((controlSwitch == false)
+		      if((tCounter > 0) && (e.which == 84)){
+			tCounter++;
+			switch(controlSwitchRCB){
+				case R:	
+					output("Next best row option is " + bestRowOptions(tCounter)[0] + ".");
+					tcounter++;
+					break;
+				case C:
+					output("Next best column option is " + bestColumnOptions(tCounter)[0] + ".");
+					tcounter++;
+					break;
+				case B:
+					output("Next best box option is " + bestBoxOptions(tCounter)[0] + ".");
+					tcounter++;
+					break;
+			}
+		      } else {
+			tCounter = 0;
+		      }
+		      switch(e.which){
+				
+                   	case 37:
                                 move("left");
                                 break;
                         case 38:
@@ -16,10 +41,10 @@
                                 move("down");
                                 break;
                         case 49:
-                                fillCurrentCell(1);
+                                fillCurrentCell("1");
                                 break;
                         case 50:
-                                fillCurrentCell(2);
+                                fillCurrentCell("2");
                                 break;
                         case 51:
                                 fillCurrentCell("3");
@@ -43,28 +68,123 @@
                                 fillCurrentCell("9");
                                 break;
 
-                   }    
-
-                });
-
-            });
-
-	$(function() {
-		var controlSwitch == false;
-		var controlSwitchR == false;
-		var controlSwitchC == false:
-		var controlSwitchB == false;
-
-		$(window).keydown(function(cs){
-		   if((controlSwitch == false)
-			switch(cs.which){
-			   case 82:
-				controlSwitchR == true;
+			case 82:
+				controlSwitch = true;
+				controlSwitchRCB = R;
 				break;
-			   case 67:
-				controlSwitchC == true;
+			case 67:
+				controlSwitch = true;
+				controlSwitchRCB = C;
 				break;
-			   case 66:	
-
+			case 66:
+				controlSwitch = true;
+				controlSwitchRCB = B;
+				break;	
 			}
-		    )
+		      )
+		      
+		   if((controlSwitch == true)
+		      switch(e.which){
+	
+                   	case 49:
+			case 50:
+			case 51:
+			case 52:
+			case 53:
+			case 54:
+			case 55:
+			case 56:
+			case 57:
+                                switch(controlSwitchRCB){
+					case R: 
+						if(contains(select("row", getCurrentRowIndex())))
+							output("Yes, row " + getCurrentRowIndex() + 1 + " contains a " + (e.which - 48) + ".");
+						else
+							output("No, row " + getCurrentRowIndex() + 1 + " does not contain a " + (e.which - 48) + ".");
+						break;
+					case C:
+						if(contains(select("column", getCurrentColumnIndex())))
+							output("Yes, column " + getCurrentColumnIndex() + 1 + " contains a " + (e.which - 48) + ".");
+						else
+							output("No, column " + getCurrentColumnIndex() + 1 + " does not contain a " + (e.which - 48) + ".");
+						break;
+					case B:
+						if(contains(select("box", getCurrentBoxIndex())))
+							output("Yes, box " + getCurrentBoxIndex() + 1 + " contains a " + (e.which - 48) + ".");
+						else
+							output("No, box " + getCurrentBoxIndex() + 1 + " does not contain a " + (e.which - 48) + ".");
+						break;
+				break;
+
+			case 82:
+                                switch(controlSwitchRCB){
+					case R:
+						var results = read(select("row", getCurrentRowIndex()));
+						var outputString = "Row " + getCurrentRowIndex() + 1 + " contains ";
+						for(var i = 0; i<9; i++)
+							outputString += results[i] + " ";
+						output(outputString);
+						break;   
+					case C:
+						var results = read(select("column", getCurrentColumnIndex()));
+						var outputString = "Column " + getCurrentColumnIndex() + 1 + " contains ";
+						for(var i = 0; i<9; i++)
+							outputString += results[i] + " ";
+						output(outputString);
+						break;   
+					case B:
+						var results = read(select("box", getCurrentBoxIndex()));
+						var outputString = "Box " + getCurrentBoxIndex() + 1 + " contains ";
+						for(var i = 0; i<9; i++)
+							outputString += results[i] + " "
+						output(outputString);
+						break;
+				break;  
+ 
+			case 77:
+                                switch(controlSwitchRCB){
+					case R:
+						var results = findMissingNumbers(select("row", getCurrentRowIndex()))
+						var outputString = "Row " + getCurrentRowIndex() + 1 + " is missing ";
+						for(var i = 0; i<results.length(); i++)
+							outputString += results[i] + " ";
+						output(outputString);
+						break;   
+					case C:
+						var results = findMissingNumbers(select("column", getCurrentColumnIndex()));
+						var outputString = "Column " + getCurrentColumnIndex() + 1 + " is missing ";
+						for(var i = 0; i<results.length(); i++)
+							outputString += results[i] + " ";
+						output(outputString);
+						break;   
+					case B:
+						var results = findMissingNumbers(select("box", getCurrentBoxIndex()));
+						var outputString = "Box " + getCurrentBoxIndex() + 1 + " is missing ";
+						for(var i = 0; i<results.length(); i++)
+							outputString += results[i] + " ";
+						output(outputString);
+						break;
+				break;  
+							
+			case 84:
+                                switch(controlSwitchRCB){
+					case R:	
+						output("Best row option is " + bestRowOptions(1)[0] + ".");
+						tcounter++;
+						break;
+					case C:
+						output("Best column option is " + bestColumnOptions(1)[0] + ".");
+						tcounter++;
+						break;
+					case B:
+						output("Best box option is " + bestBoxOptions(1)[0] + ".");
+						tcounter++;
+						break;
+				
+				break;
+
+			controlSwitch = false;
+			
+		   });
+	
+		});
