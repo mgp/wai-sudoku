ae338335557a8f668e6e5b0c4358999ec317b5c5
Maria Pavlovskaia <Maria Pavlovskaia@Messages-MacBook-Pro.local>
2012-02-27 22:24:58 -0800

    made it firefox compatible but also less cool in that browser

--- movecell.js
+++ movecell.js
@@ -1,3 +1,4 @@
+            var isChrome = /Chrome/.test(navigator.userAgent);
 
 
             function move(movt)
@@ -11,7 +12,7 @@
                 var newColChar;
                 var cellnew="beep";
                 crossedBox=0;
-                if(movt=="up")
+                if(movt == "up")
                 {
                      if(row!='a') //cant go above
                      {
@@ -31,55 +32,62 @@
                 {
                      if(row!='i') //cant go below
                      {
-                        newRow=row.charCodeAt(0);
-                        newRow=newRow+1;
-                        newRowChar=String.fromCharCode(newRow);
-                        cellnew=newRowChar+col;
-                        if(parseInt(getRowCoordinate(cellnew)/3)!=parseInt(getRowCoordinate(cell)/3))//it crossed a box
+                        newRow = row.charCodeAt(0);
+                        newRow = newRow + 1;
+                        newRowChar = String.fromCharCode(newRow);
+                        cellnew = newRowChar + col;
+                        if (parseInt(getRowCoordinate(cellnew)/3) != parseInt(getRowCoordinate(cell)/3))//it crossed a box
                         {
-                            crossedBox=1;
+                            crossedBox = 1;
                         }
                      }
                 }
-                else if(movt=="right")
+                else if(movt == "right")
                 {
-                     if(col!='9')
+                     if(col != '9')
                      {
-                        newCol=col.charCodeAt(0);
-                        newCol=newCol+1;
-                        newColChar=String.fromCharCode(newCol);
-                        cellnew=row+newColChar;
-                        if(parseInt(getColCoordinate(cellnew)/3)!=parseInt(getColCoordinate(cell)/3))//it crossed a box
+                        newCol = col.charCodeAt(0);
+                        newCol = newCol + 1;
+                        newColChar = String.fromCharCode(newCol);
+                        cellnew = row + newColChar;
+                        if (parseInt(getColCoordinate(cellnew)/3) != parseInt(getColCoordinate(cell)/3))//it crossed a box
                         {
-                            crossedBox=1;
+                            crossedBox = 1;
                         }
                      }
                 }
-                else if(movt=="left")
+                else if(movt == "left")
                 {
-                    if(col!=1)
+                    if(col != 1)
                      {
-                        newCol=col.charCodeAt(0);
-                        newCol=newCol-1;
-                        newColChar=String.fromCharCode(newCol);
-                        cellnew=row+newColChar;
-                        if(parseInt(getColCoordinate(cellnew)/3)!=parseInt(getColCoordinate(cell)/3))//it crossed a box
+                        newCol = col.charCodeAt(0);
+                        newCol = newCol - 1;
+                        newColChar = String.fromCharCode(newCol);
+                        cellnew = row + newColChar;
+                        if (parseInt(getColCoordinate(cellnew)/3) != parseInt(getColCoordinate(cell)/3))//it crossed a box
                         {
-                            crossedBox=1;
+                            crossedBox = 1;
                         }
                      }
                 }
                     moveBetweenCells(cell,cellnew);
 
             }
+
+
             function color(cell, colr) {
                 var cellNode = document.getElementById(cell);
                 var num = cellNode.innerHTML;
                 if (colr == 0) {
-                    cellNode.className = cellNode.className.replace(/highlighted/, '');
+                    $(cellNode).removeClass('highlighted');
+                    if (isChrome) {
+                        $(cellNode).removeClass('chrome');
+                    }
+                    
                 } else if (colr == 1) {
-                    if (!/highlighted/.test(cellNode.className)) {
-                        cellNode.className += ' highlighted';
+                    $(cellNode).addClass('highlighted');
+                    if (isChrome) {
+                        $(cellNode).addClass('chrome');
                     }
 
                     var outstring = "";
